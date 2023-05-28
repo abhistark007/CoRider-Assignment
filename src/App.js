9a416f6f6c51d48758b7296629b18ede0ae26c84
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-28 15:40:09 +0530

    create ApiTesting function to test api response

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchChats } from './controllers/api';
+import { toast } from 'react-toastify';
+import { fetchChatsResults } from './store/chatsSlice';
 
 function App() {
+
+  const dispatch=useDispatch();
+  const {value}=useSelector((state)=>state.chat);
+
+  const apiTesting=async()=>{
+    const data=await  fetchChats(0);
+
+    if(data!==false){
+      console.log("DATA-> ",data.data);
+      dispatch(fetchChatsResults(data.data))
+    }
+  }
+
+  useEffect(()=>{
+    apiTesting();
+  },[])
+
+
   return (
     <div className="App">
       
