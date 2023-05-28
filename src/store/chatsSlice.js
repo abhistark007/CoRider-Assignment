35afd8cae9db5c94a90b71146a106a01680a1452
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-28 15:41:08 +0530

    Update chatsSlice.js
    Created from,message,name,status,to state variable

--- src/store/chatsSlice.js
+++ src/store/chatsSlice.js
@@ -4,10 +4,20 @@ export const chatsSlice = createSlice({
   name: 'chat',
   initialState: {
     value: {},
+    from:"",
+    message:"",
+    name:"",
+    status:"",
+    to:""
   },
   reducers: {
     fetchChatsResults: (state,action) => {
-      state.value=action.payload;
+      state.value=action.payload.chats;
+      state.from=action.payload.from;
+      state.message=action.payload.message;
+      state.name=action.payload.name;
+      state.status=action.payload.status;
+      state.to=action.payload.to;
     },
     addChatsResults: (state,action)=>{
         state.value.push(action.payload);
@@ -16,6 +26,6 @@ export const chatsSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { fetchShortURLResults,addChatsResults } = chatsSlice.actions
+export const { fetchChatsResults,addChatsResults } = chatsSlice.actions
 
 export default chatsSlice.reducer
