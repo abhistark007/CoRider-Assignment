14136b8c61fdd6b6f521f444ea94226b1aaab3fa
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-28 15:23:54 +0530

    Update api.js
    Created a function to fetch chats for a given page number

---
+++ src/controllers/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+
+const BASE_URL="http://3.111.128.67/assignment/chat?page=";
+
+export const fetchChats=async(pageNumber)=>{
+    try{
+        const data=await axios.get(BASE_URL+pageNumber);
+        // console.log("SSSS",data);
+        toast("Chat Fetched Successfully");
+        return data;
+    }catch(e){
+        // console.log(e.response.data.ok);
+        toast.error(e.response.data.error);
+        return e.response.data.ok;
+    }
+}
