7529bf8e62fa23716b75def2f2d3de6f46e3b674
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-28 21:45:15 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Footer.css'
 import pinIcon from '../assets/pinIcon.png';
 import sendIcon from '../assets/sendIcon.png';
+import camIcon from '../assets/camIcon.png';
+import videoIcon from '../assets/videoIcon.png';
+import docIcon from '../assets/docIcon.png';
 
 function Footer({to}) {
+  const [popup,setPopup]=useState(true);
   return (
     <div className='footer'>
         <input type="text" placeholder={`Reply to @${to}`}/>
         <div className="footer__icons">
-          <img src={pinIcon} alt='pinIcon'/>
+          {
+            popup?<></>:
+            (<div className="footer__popup">
+              <img src={camIcon} alt="camIcon" />
+              <img src={videoIcon} alt="videoIcon" />
+              <img src={docIcon} alt="docIcon" />
+            </div>)
+          }
+          <img onClick={()=>setPopup(!popup)} src={pinIcon} alt='pinIcon'/> 
           <img src={sendIcon} alt="sendIcon" />
         </div>
     </div>
