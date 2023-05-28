100d95e84ea8ed45c9ba4e0d278e054833882b86
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-28 20:31:52 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Footer.css'
+import pinIcon from '../assets/pinIcon.png';
+import sendIcon from '../assets/sendIcon.png';
 
-function Footer() {
+function Footer({to}) {
   return (
     <div className='footer'>
-        
+        <input type="text" placeholder={`Reply to @${to}`}/>
+        <div className="footer__icons">
+          <img src={pinIcon} alt='pinIcon'/>
+          <img src={sendIcon} alt="sendIcon" />
+        </div>
     </div>
   )
 }
