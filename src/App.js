60300f57aad5b401970ac8290a942223edb1f569
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-29 15:05:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { fetchChats } from './controllers/api';
 import InfiniteScroll from 'react-infinite-scroll-component';
@@ -8,50 +8,82 @@ import Footer from './components/Footer';
 
 function App() {
 
-  const [from,setFrom]=useState("");
-  const [to,setTo]=useState("");
-  const [title,setTitle]=useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [title, setTitle] = useState("");
+  const scrollableDivRef = useRef(null);
 
-  const [chats,setChats]=useState([]);
-  const [page,setPage]=useState(0);
+  const [chats, setChats] = useState([]);
+  const [page, setPage] = useState(0);
 
-  const apiTesting=async()=>{
-    const data=await  fetchChats(0);
+  const apiTesting = async () => {
+    const data = await fetchChats(0);
 
-    if(data!==false){
-      console.log("DATA-> ",data.data);
+    if (data !== false) {
+      console.log("DATA-> ", data.data);
       // dispatch(fetchChatsResults(data.data))
-      setChats([...data.data.chats]);
-      setFrom(data.data.from);
-      setTo(data.data.to);
-      setTitle(data.data.name);
+      setChats(n=>[...data.data.chats]);
+      setFrom(n=>data.data.from);
+      setTo(n=>data.data.to);
+      setTitle(n=>data.data.name);
     }
   }
 
-  const dum=async()=>{
-    setPage(page+1);
-    const data=await  fetchChats(page);
-    
-    if(data!==false){
-      console.log(`DATA page ${page}-> `,data.data);
+  const dum = async () => {
+    setPage(page=>page + 1);
+    const data = await fetchChats(page);
+
+    if (data !== false) {
+      console.log(`DATA page ${page}-> `, data.data);
       // dispatch(addChatsResults(data.data))
-      setChats([...chats,...data.data.chats]);
+      setChats([...chats, ...data.data.chats]);
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     apiTesting();
-  },[])
+    // scrollToBottom();
+  }, [])
+
+
+  const handleScroll = () => {
+    const { scrollTop } = scrollableDivRef.current;
+
+    // Check if user has scrolled to the top
+    if (scrollTop === 0) {
+      dum();
+    }
+  };
+
+  // const scrollToBottom = () => {
+  //   const scrollableDiv = scrollableDivRef.current;
+  //   scrollableDiv.scrollIntoView({ block: "end" });
+  // };
 
 
   return (
     <div className="App">
-       
-        <Header to={to} from={from} title={title}/>
+
+      <Header to={to} from={from} title={title} />
+      <div className='app__body'
+      style={{
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        overflowY: 'auto',
+        maxHeight: '100vh',
         
+      }}
+      onScroll={handleScroll}
+      ref={scrollableDivRef}
+      >
         <InfiniteScroll
+          style={{display:"flex", flexDirection:"column-reverse",}}
           dataLength={chats.length} //This is important field to render the next data
           next={dum}
+          scrollToBottom={true}
+          // style={{ display: 'flex', flexDirection: 'column-reverse' }} //To put endMessage and loader to the top.
+          // inverse={true} //
+          
           hasMore={true}
           loader={<h4>Loading...</h4>}
           endMessage={
@@ -59,19 +91,26 @@ function App() {
               <b>Yay! You have seen it all</b>
             </p>
           }
-          >
-            <div className='app__body'>
-            
+          inverse={true}
+          useWindow={false}
+          scrollableTarget="scrollableDiv"
+        >
+          
+
             {
-              chats.map((chat,index)=>{
-                return <ChatCart key={index} isKycVerified={chat.sender.is_kyc_verified} message={chat.message} pic={chat.sender.image} self={chat.sender.self} />
+              chats.map((chat, index) => {
+                if(index===0){
+                  return <div style={{marginBottom:"400px"}}><ChatCart key={index} index={index} isKycVerified={chat.sender.is_kyc_verified} message={chat.message} pic={chat.sender.image} self={chat.sender.self} /></div>
+                }
+                return <ChatCart key={index} index={index} isKycVerified={chat.sender.is_kyc_verified} message={chat.message} pic={chat.sender.image} self={chat.sender.self} />
               })
             }
-            </div>
-          </InfiniteScroll>
+          
+        </InfiniteScroll>
 
-          <Footer to={to}/>
-       
+        </div>
+      <Footer to={to} />
+
     </div>
   );
 }
